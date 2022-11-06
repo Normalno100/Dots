61ebf2518e7a06203065642f17921856d5ae45a3
Maksim Somsin <msomsin@mail.ru>
2022-11-06 11:54:51 +0300

    '06/11'

--- src/Test2.java
+++ src/Test2.java
@@ -6,8 +6,8 @@ import java.time.format.DateTimeFormatter;
 public class Test2 {
     public static void main(String[] args) {
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy, HH:mm:ss");
-        String text = dtf.format( LocalDateTime.now() );
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy, HH:mm:ss");
+        String text = time.format( LocalDateTime.now() );
         System.out.println(text);
 
 
